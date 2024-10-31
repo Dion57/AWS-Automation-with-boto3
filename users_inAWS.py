b91501c3d45f01ced2be22da7f1840eee886308f
dion <deonnichole57@gmail.com>
2024-10-31 17:52:09 +0100

    new

--- users_inAWS.py
+++ users_inAWS.py
@@ -5,3 +5,8 @@ iam_console = aws_management_console.resource('iam')
 
 for each_user in iam_console.users.all():
     print(each_user.name)
+
+
+    #The session is the AWS management console. It saves configurations and allows us create services, clients and resources.
+    #Resources and Clients enables you to create a particular AWS Service. 
+    #You can also specifywhich region where you want the service to be created
