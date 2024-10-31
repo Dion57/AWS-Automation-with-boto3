2db37ded410d8306c89a89f277073e5bb4e34ebf
dion <deonnichole57@gmail.com>
2024-10-31 20:05:12 +0100

    Reservations

--- ec2_in_aws.py
+++ ec2_in_aws.py
@@ -5,5 +5,5 @@ aws_management_console = boto3.session.Session(profile_name="default")
 # open ec2 console
 ec2_console = aws_management_console.client(service_name='ec2')
 # To list instances
-list_instances = ec2_console.describe_instances()
+list_instances = ec2_console.describe_instances()['Reservations']
 print(list_instances)
