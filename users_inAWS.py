7389805234f007f0fc92e59fb7420886beb57f36
dion <deonnichole57@gmail.com>
2024-10-31 18:16:38 +0100

    new

--- users_inAWS.py
+++ users_inAWS.py
@@ -1,12 +1,24 @@
 import boto3
 
 aws_management_console = boto3.session.Session(profile_name="default")
-iam_console = aws_management_console.resource('iam')
+iam_console_resource = aws_management_console.resource('iam')
+iam_console_client = aws_management_console.client('iam')
 
+# IAM Users list using resource object:
 for each_user in iam_console.users.all():
     print(each_user.name)
 
+# IAM  Users list using client object:
+for each in iam_console_client.list_users()['users']:
+    print(each['userName'])
 
-    #The session is the AWS management console. It saves configurations and allows us create services, clients and resources.
-    #Resources and Clients enables you to create a particular AWS Service. 
-    #You can also specifywhich region where you want the service to be created
+"""
+    The session is the AWS management console. It saves configurations and allows us create services, clients and resources.
+    Resources and Clients enables you to create a particular AWS Service. 
+    You can also specifywhich region where you want the service to be created
+    To get the resource objects: 
+    aws_management_console = boto3.session.Session(profile_name="default")
+    dir(aws_management_console)
+    print(aws_management_console.get_available_resources())
+    All Operations on the AWS management console can be done using the CLIENT whereas, Resources does not guaratee that. It is not supported for all the services.
+    """
