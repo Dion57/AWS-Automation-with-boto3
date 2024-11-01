5bad0457e6d65414d9b320fc87ce3959bb9b4a60
dion <deonnichole57@gmail.com>
2024-11-01 13:12:56 +0100

    create instance

--- create_ec2_instance.py
+++ create_ec2_instance.py
@@ -5,4 +5,9 @@ aws_management_console = boto3.session.Session(profile_name="default")
 # open ec2 console
 ec2_console = aws_management_console.client('ec2')
 # create the ec2 instance
-create_instance = ec2_console.run_Instance()
+create_instance = ec2_console.run_instances(
+    ImageId='ami-###################',
+    InstanceType='t2.micro',
+    MaxCount=1,
+    MinCount=1,
+)
